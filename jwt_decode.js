7934f807cafd998f35741285c8138c5f7e549d49
Nadeem Sajjad <info@easeit.in>
2016-04-28 12:19:00 +0530

    Create jwt_decode.js

---
+++ jwt_decode.js
@@ -0,0 +1,15 @@
+/*
+  To decode JWT token and get data as JSON object (Only to get data - Not useful to verify or authenticate)
+*/
+decodeJwtToken = function(t) {
+	var splits=t.split(".");
+	var s=splits[1];
+	var e={},i,b=0,c,x,l=0,a,r='',w=String.fromCharCode,L=s.length;
+	var A="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+	for(i=0;i<64;i++){e[A.charAt(i)]=i;}
+	for(x=0;x<L;x++){
+		c=e[s.charAt(x)];b=(b<<6)+c;l+=6;
+		while(l>=8){((a=(b>>>(l-=8))&0xff)||(x<(L-2)))&&(r+=w(a));}
+	}
+	return JSON.parse(r);
+};
